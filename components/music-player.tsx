78d73f6962cfee22bcc03860e2bf88d46f97220c
[dyad] <toyesterdayagencia@gmail.com>
2025-09-25 21:14:54 -0300

    [dyad] Corrigido o loop da música no player - wrote 1 file(s)

--- components/music-player.tsx
+++ components/music-player.tsx
@@ -106,7 +106,7 @@ export function MusicPlayer() {
       >
         {isMuted ? <VolumeX size={20} /> : <Volume2 size={20} />}
       </Button>
-      <audio ref={audioRef} loop preload="metadata" onEnded={() => setIsPlaying(false)}>
+      <audio ref={audioRef} preload="metadata" onEnded={() => setIsPlaying(false)}>
         <source src="/pvc.mp3" type="audio/mpeg" />
       </audio>
       {hasError && (
